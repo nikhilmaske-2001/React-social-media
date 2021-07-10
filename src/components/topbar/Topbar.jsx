961bd8ea9b70eef28c5667773bb661b6750981f6
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-10 11:23:31 +0530

    Topbar style

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,36 @@
 import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 
 export default function Topbar() {
     return (
         <div className="topbarContainer">
-            <div className="topbarLeft"></div>
+            <div className="topbarLeft">
+                <span className="logo">LamaSocial</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <Search className="searchIcon"/>
+                    <input placeholder="Search for friends, post or video" className="searchInput"/>
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">HomePage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem"></div>
+                    <Person/>
+                    <span className="topbarIconBadge">1</span>
+                    <div className="topbarIconItem"></div>
+                    <Chat/>
+                    <span className="topbarIconBadge">1</span>
+                    <div className="topbarIconItem"></div>
+                    <Notifications/>
+                    <span className="topbarIconBadge">1</span>
+                </div>
+                <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+            </div>
         </div>
     )
 }

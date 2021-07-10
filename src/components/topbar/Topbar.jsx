0666aef747fcad0a9aa88137ac5a0fc3cd0ba05a
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-10 10:49:54 +0530

    Basic structuring of files.

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,10 @@
+import "./topbar.css";
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft"></div>
+        </div>
+    )
+}
+
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,8 @@
+import Topbar from "../../components/topbar/Topbar";
+
 export default function Home() {
     return (
-        <div>
-            this is home js
-        </div>
+        <Topbar/>
     )
 }
 

8719041c32c7b2e464b261bb24da27571e914a5f
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-10 11:39:31 +0530

    Fix topbar notificatopn badge placement.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -19,15 +19,18 @@ export default function Topbar() {
                     <span className="topbarLink">Timeline</span>
                 </div>
                 <div className="topbarIcons">
-                    <div className="topbarIconItem"></div>
-                    <Person/>
-                    <span className="topbarIconBadge">1</span>
-                    <div className="topbarIconItem"></div>
-                    <Chat/>
-                    <span className="topbarIconBadge">1</span>
-                    <div className="topbarIconItem"></div>
-                    <Notifications/>
-                    <span className="topbarIconBadge">1</span>
+                    <div className="topbarIconItem">
+                        <Person/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Chat/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
                 </div>
                 <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
             </div>

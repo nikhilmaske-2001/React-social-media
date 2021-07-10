cbded91969960f071101fbee41f1bb80c2905a79
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-10 15:13:14 +0530

    Created RIghtbar component.

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,15 @@
 import "./rightbar.css";
-
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            Rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg" />
+                    <span className="birthdayText"> <b>Sinchan</b> and <b>2 other friends</b> have a birthday today</span>
+                </div>
+            </div>
+            <img src="assets/ad.png" alt="" className="rightbarAd" />
         </div>
     )
 }
+

9ccde1d4e386037ec1c5a20d00781bbb0509a015
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-11 11:57:55 +0530

    Add paramters from dummydata

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,5 +1,6 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
+import {Users} from "../../dummyData";
 
 export default function Post({ post }) {
   return (
@@ -9,29 +10,29 @@ export default function Post({ post }) {
           <div className="postTopLeft">
             <img
               className="postProfileImg"
-              src="/assets/person/1.jpeg" alt=""
+              src={Users.filter((u) => u.id === post?.userId)[0].profilePicture} alt=""
             />
             <span className="postUsername">
-              sinchan
+              {Users.filter((u) => u.id === post?.userId)[0].username}
             </span>
-            <span className="postDate">5 mins ago</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">Hey its my first post</span>
-          <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className="likeIcon" src="assets/like.png" alt="" />
             <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">32 people like it</span>
+            <span className="postLikeCounter">{post.like} people like it</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText"> comments</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>

5c49a08d863ed4721a21d10b91cefe49f6b8584a
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-11 14:34:06 +0530

    Profile page created.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Home from "./pages/home/Home";
+import Profile from "../src/pages/profile/Profile"
 
 function App() {
   return (
-      <Home/>
+      // <Home/>
+      <Profile/>
   );
 }
 

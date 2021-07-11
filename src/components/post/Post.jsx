9865bd9950bd9b3ca3f0d9bfd13349de827712de
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-11 14:12:49 +0530

    Add like functionality

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,23 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
 import {Users} from "../../dummyData";
+import { useState } from "react";
 
 export default function Post({ post }) {
+  // Like functionality
+  // take the like count from the dummy data
+  const [like, setLike] = useState(post.like);
+  // Initially put like = false (no like already)
+  const [isLiked, setIsLiked] = useState(false);
+
+  // Onclick like button
+  const likeHandler = () => {
+    // if already like then dislike, else like
+    setLike(isLiked ? like-1 : like+1);
+    // flip the isliked
+    setIsLiked(!isLiked);
+  }
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -27,9 +42,9 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt="" />
-            <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">{post.like} people like it</span>
+            <img className="likeIcon" src="assets/like.png" onClick = {likeHandler}  alt="" />
+            <img className="likeIcon" src="assets/heart.png" onClick = {likeHandler} alt="" />
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>

b65462d2e5d205f510f29d13c5f8190b06f8b2b7
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-14 18:47:55 +0530

    Add routes to move between other pages.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  return <Profile/>;
+  return(
+    // Using Router to switch between pages
+    <Router>
+      <Switch>
+        {/* Using 'exact' to remove the confusion between '/' & 'xyz'. Removing it results into not working of other pages */}
+        <Route exact path = "/">
+          <Home/>
+        </Route>
+        <Route path = "/login">
+          <Login/>
+        </Route>
+        <Route path = "/profile/:safak">
+          <Profile/>
+        </Route>
+        <Route path = "/register">
+          <Register/>
+        </Route>
+      </Switch>
+    </Router>  
+  );
 }
 
 export default App;

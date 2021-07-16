9604d264b3186be79d0acab2c4ecb7d2cb1acfcc
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-16 18:57:44 +0530

    Using axios to fetch data from the api (not working).

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,16 +1,29 @@
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import {Posts} from "../../dummyData";
+import axios from "axios";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  // 1. Call this useEffect when the Feed() is called only, that why [] is used at last
+  // 2. We will need to use async and await but we can't user it in useEffect, so making another function inside it
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("posts/timeline/60e0705881fa36f09d5307d3");
+      console.log(res);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
+        {/* {Posts.map((p) => (
           <Post key={p.id} post = {p}/>
-        ))}
+        ))} */}
       </div>
     </div>
   );
--- src/components/online/Online.jsx
+++ src/components/online/Online.jsx
@@ -5,12 +5,12 @@ export default  function Online({user}) {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
     return (
-    <l1 className="rightbarFriend">
+    <li className="rightbarFriend">
         <div className="rightbarProfileImgContainer">
             <img src={PF+user.profilePicture} alt="" className="rightbarProfileImg" />
         <span className="rightbarOnline"></span>
         </div>
         <span className="rightbarUsername">{user.username}</span>
-    </l1>
+    </li>
     )
 }

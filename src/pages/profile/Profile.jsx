9614e4c2241fc2f908060f1f9cc2b32245909b01
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-20 15:24:26 +0530

    Show user details from db.

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,10 +3,21 @@ import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Profile() {
   // PUBLIC FOLDER
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/user?username=Nikhil`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, []);
 
   return (
     <>
@@ -28,8 +39,8 @@ export default function Profile() {
               />
             </div>
             <div className="profileInfo">
-              <h4 className="profileInfoName">Safak Kocaoglu</h4>
-              <span className="profileInfoDesc">Hello my friends!</span>
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc">{user.desc}</span>
             </div>
           </div>
           <div className="profileRightBottom">

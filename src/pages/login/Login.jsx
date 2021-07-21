44d94a015f962def1a24967dd5f2efa32bca192c
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-21 17:47:05 +0530

    Enhance login page.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,14 @@
+import { useRef } from "react";
 import "./login.css";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  // Writing e in the bracket to avoid loading of page after function call
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+  };
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,15 +19,28 @@ export default function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Email"
+              type="email"
+              required
+              className="loginInput"
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              required
+              minLength="6"
+              type="password"
+              className="loginInput"
+              ref={password}
+            />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>

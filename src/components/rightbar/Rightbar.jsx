be89f7967efc8b0eb286c97044ec2d76e8cd278c
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-22 17:45:16 +0530

    UseEffect for feed component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,18 +3,18 @@ import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
 export default function Rightbar({ profile }) {
-   // PUBLIC FOLDER
-   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  // PUBLIC FOLDER
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const HomeRightbar = () => {
     return (
       <>
         <div className="birthdayContainer">
-          <img className="birthdayImg" src={`${PF}gift.png" alt=""`} />
+          <img className="birthdayImg" src={`${PF}gift.png`} alt="" />
           <span className="birthdayText">
             <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
           </span>
         </div>
-        <img className="rightbarAd" src={`${PF}ad.png" alt=""`} />
+        <img className="rightbarAd" src={`${PF}ad.png`} alt="" />
         <h4 className="rightbarTitle">Online Friends</h4>
         <ul className="rightbarFriendList">
           {Users.map((u) => (
@@ -26,7 +26,7 @@ export default function Rightbar({ profile }) {
   };
 
   const ProfileRightbar = () => {
-     // PUBLIC FOLDER
+    // PUBLIC FOLDER
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
     return (

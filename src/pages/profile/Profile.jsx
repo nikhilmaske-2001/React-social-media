9620cd38ee0d2826eb9e7bbc2611b5383bbbb2a2
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-22 19:08:44 +0530

    Show user profile data from db.

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,19 +5,21 @@ import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router";
 
 export default function Profile() {
   // PUBLIC FOLDER
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({});
+  const username = useParams().username;
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await axios.get(`/users?username=Nikhil`);
+      const res = await axios.get(`/users?username=${username}`);
       setUser(res.data);
     };
     fetchUser();
-  }, []);
+  }, [username]);
 
   return (
     <>
@@ -29,12 +31,12 @@ export default function Profile() {
             <div className="profileCover">
               <img
                 className="profileCoverImg"
-                src={`${PF}post/3.jpeg`}
+                src={user.coverPicture || PF + "person/noCover.png"}
                 alt=""
               />
               <img
                 className="profileUserImg"
-                src={`${PF}/person/7.jpeg`}
+                src={user.profilePicture || PF + "person/noAvatar.png"}
                 alt=""
               />
             </div>
@@ -44,8 +46,8 @@ export default function Profile() {
             </div>
           </div>
           <div className="profileRightBottom">
-            <Feed username="Nikhil" />
-            <Rightbar profile />
+            <Feed username={username} />
+            <Rightbar user={user} />
           </div>
         </div>
       </div>

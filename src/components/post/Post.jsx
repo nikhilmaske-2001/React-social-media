a73cad6b2574fc194a634fab9ab7b0fd868c38c1
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-22 18:29:34 +0530

    Link to profile page.

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -3,6 +3,7 @@ import { MoreVert } from "@material-ui/icons";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { format } from "timeago.js";
+import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
   // Like functionality
@@ -37,11 +38,14 @@ export default function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              className="postProfileImg"
-              src={user.profilePicture || PF + "person/noAvatar.png"}
-              alt=""
-            />
+            <Link to={`profile/${user.username}`}>
+              <img
+                className="postProfileImg"
+                src={user.profilePicture || PF + "person/noAvatar.png"}
+                alt=""
+              />
+            </Link>
+
             <span className="postUsername">{user.username}</span>
             <span className="postDate">{format(post.createdAt)}</span>
           </div>

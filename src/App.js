e705ddc26d97af3f5fbc6e3339bb75e2cca30028
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-24 14:18:12 +0530

    Schedule the pages according to the status.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,29 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const {user} = useContext(AuthContext);
   return(
     // Using Router to switch between pages
     <Router>
       <Switch>
         {/* Using 'exact' to remove the confusion between '/' & 'xyz'. Removing it results into not working of other pages */}
         <Route exact path = "/">
-          <Home/>
+          {user ? <Home/>: <Register/>}
         </Route>
         <Route path = "/login">
-          <Login/>
+          {user ? <Redirect to="/"/> : <Login/>}
         </Route>
         <Route path = "/profile/:username">
           <Profile/>
         </Route>
         <Route path = "/register">
+        {user ? <Redirect to="/"/> : <Login/>}
           <Register/>
         </Route>
       </Switch>

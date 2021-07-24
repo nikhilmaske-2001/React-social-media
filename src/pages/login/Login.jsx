9cd44b69b747a1004d9f8a7f1c72269e75c297dc
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-24 13:58:34 +0530

    Login system with context api: Part 1.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,22 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Login() {
   const email = useRef();
   const password = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
   const handleClick = (e) => {
     e.preventDefault();
-    console.log(email.current.value);
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
   };
+
+  console.log(user);
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -34,7 +43,9 @@ export default function Login() {
               ref={password}
               minLength="6"
             />
-            <button className="loginButton">Log In</button>
+            <button className="loginButton" type="submit">
+              {isFetching ? "loading" : "Log In"}
+            </button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
